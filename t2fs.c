883666ed5e46acd1fc49dae544aaa9aa0ca11b2e
ifschilling <ianfschilling@gmail.com>
2018-05-22 00:52:34 -0300

    Create t2fs.c

---
+++ t2fs.c
@@ -0,0 +1,14 @@
+/* Arthur Böckmann Grossi (275607), Cassiano Translatti Furlani(278038) e Ian Fischer Schilling(275603)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/apidisk.h"
+#include "../include/bitmap2.h"
+#include "../include/t2fs.h"
+
+#define MAX_STU_CHAR 100
+#define STU1 "Arthur Bockmann Grossi - 275607"
+#define STU2 "Cassiano Translatti Furlani - 278038"
+#define STU3 "Ian Fischer Schilling - 275603"
