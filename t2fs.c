aebffee6a6c31cb37e4881b9ba5ba2913988a738
ifschilling <ianfschilling@gmail.com>
2018-05-22 01:08:14 -0300

    Update t2fs.c

--- t2fs.c
+++ t2fs.c
@@ -12,3 +12,35 @@
 #define STU1 "Arthur Bockmann Grossi - 275607"
 #define STU2 "Cassiano Translatti Furlani - 278038"
 #define STU3 "Ian Fischer Schilling - 275603"
+
+
+int cidentify(char *name, int size){
+	char student[MAX_STU_CHAR] = "";
+	int i;
+	int st2 = strlen(STU1);
+	int st3 = st2 + strlen(STU2);
+	int letters;
+	strcat(student, STU1);
+	strcat(student, STU2);
+	strcat(student, STU3);
+	if(size >= MAX_STU_CHAR) //Size is equal to or greater than students names
+	{
+		for( i = 0; i < MAX_STU_CHAR;i++)
+		name[i] = student[i];
+		return 0;
+	}
+	else if( size >= 9) //At least 1 characters for each
+	{
+		letters = (int)size/3 - 2;
+		for(i = 0; i < letters; i++)
+		{
+			name[i] = student[i];
+			name[i + letters + 1] = student[i + st2];
+			name[i + 2*(letters + 1)] = student[i + st3];
+		}
+		name[letters] = '\n';
+		name[2*(letters) +1]= '\n';
+		name[i + 2*(letters + 1) + 1] = '\0';
+	}
+	return -1;
+}
