e4341126c7c4c082b6c671d12400d9a592992aa7
Fokendov <38194317+Fokendov@users.noreply.github.com>
2018-05-30 17:12:37 -0300

    setup_changes

--- t2fs.c
+++ t2fs.c
@@ -1,255 +1,133 @@
-/* Arthur Böckmann Grossi (275607), Cassiano Translatti Furlani(278038) e Ian Fischer Schilling(275603)*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "../include/t2fs.h"
 #include "../include/bitmap2.h"
 #include "../include/apidisk.h"
-/* talvez precise mais includes */
 
+
+#define MAX_NAME_SIZE 256
 /* STRUCTURES*/
 typedef struct files
 {	
-	char *fileName;
+	char fullPathName[MAX_NAME_SIZE];
+	BYTE type;
 	int currentPointer;
 	int handle;
-	
-	
+	int size;
+
 }fileHandler;
 
+typedef struct cD{
+	char pathName[MAX_NAME_SIZE];
+	int block;
+}dirDescription;
+
 /* GLOBAL VARIABLES*/
+
+struct t2fs_superbloco *superBlock;
 int systemReady = 0;
 BYTE buffer[SECTOR_SIZE];
-struct t2fs_superbloco superBlock;
+dirDescription *currentDir;
 
-WORD sectorsByBlock;
-DWORD numberOfTotalBLocks;
-WORD numBlocksINode;
+int bytesInBlock;
+int firstDataBlock;
+int inodeSector;
 
-WORD freeInodeBitmap;
-WORD freeBlocksBitmap; 
+fileHandler *openedFiles[10];
 
 /* CREATED FUNCTIONS */
 int setup();
 
 
-
-
-int identify2(char *name, int size){
-	char student[MAX_STU_CHAR] = "";
-	int i;
-	int st2 = strlen(STU1);
-	int st3 = st2 + strlen(STU2);
-	int letters;
-	strcat(student, STU1);
-	strcat(student, STU2);
-	strcat(student, STU3);
-	if(size >= MAX_STU_CHAR) //Size is equal to or greater than students names
-	{
-		for( i = 0; i < MAX_STU_CHAR;i++)
-		name[i] = student[i];
-		return 0;
-	}
-	else if( size >= 9) //At least 1 characters for each
-	{
-		letters = (int)size/3 - 2;
-		for(i = 0; i < letters; i++)
-		{
-			name[i] = student[i];
-			name[i + letters + 1] = student[i + st2];
-			name[i + 2*(letters + 1)] = student[i + st3];
-		}
-		name[letters] = '\n';
-		name[2*(letters) +1]= '\n';
-		name[i + 2*(letters + 1) + 1] = '\0';
-	}
-	return -1;
-}
-
-FILE2 create2 (char *filename){
-	/* Alocar memoria para armazenar o registro do novo arquivo */
-	struct t2fs_record *registro;
-	registro = calloc(1, sizeof(t2fs_record));
-
-	/* Inicializar registro */
-	registro->TypeVal = TYPEVAL_REGULAR;
-	registro->name = filename;
-	registro->inodeNumber = ;
-
-
-}
-
-int delete2 (char *filename){
-
-}
-
-FILE2 open2 (char *filename){
-
-}
-
-int close2 (FILE2 handle){
-
-}
-
-int read2 (FILE2 handle, char *buffer, int size){
-
-}
-
-int write2 (FILE2 handle, char *buffer, int size){
-
-}
-
-int truncate2 (FILE2 handle){
-
-}
-
-int seek2 (FILE2 handle, DWORD offset){
-
-}
-
-int mkdir2 (char *pathname){
-
-}
-
-int rmdir2 (char *pathname){
-
-}
-
-int chdir2 (char *pathname){
-
-}
-
-int getcwd2 (char *pathname, int size){
-
-}
-
-DIR2 opendir2 (char *pathname){
-
-}
-
-int readdir2 (DIR2 handle, DIRENT2 *dentry){
-
-}
-
-int closedir2 (DIR2 handle){
-
-}
+/* 
+Funtion: initialises the file system by reading the superblock
+Return 	0 if successful
+		-1 if error	
+*/
 
 int setup(){
 	//Create root directory and prepare the file
-	if( (read_sector(0, &buffer)) ){
+	if( (read_sector(0, buffer)) ){
 		printf("Could not read first sector\n");
 		return -1;
 	}
-	else{
-		char idT2FS[4];
-		char byte, word[2], dword[4];
 
-		strncpy(idT2FS, buffer, 4); //ID
-		if( strcmp(idT2FS, "T2FS") ){
-			printf("Error, not T2FS\n");
-			return -1;
-		}
-		else{
-			strcpy(word, buffer[8]);
-			strcat(word, buffer[9]);
+	superBlock = calloc(1, sizeof(struct t2fs_superbloco));
 
-			freeBlocksBitmap = (WORD)atoi(word);
-
-			strcpy(word, buffer[10]);
-			strcat(word, buffer[11]);
-			
-			freeInodeBitmap = (WORD)atoi(word);
-
-			strcpy(word, buffer[12]);
-			strcat(word, buffer[13]);
-			
-			numBlocksINode = (WORD)atoi(word);
-
-			strcpy(word, buffer[14]);
-			strcat(word, buffer[15]);
-			
-			sectorsByBlock= (WORD)atoi(word);
-
-			strcpy(word, buffer[16]);
-			strcat(word, buffer[17]);
-			strcat(word, buffer[18]);
-			strcat(word, buffer[19]);
-			
-			numberOfTotalBLocks = atoi(word);
-
-
-			int firstDataSector = (numBlocksINode + freeInodeBitmap + freeBlocksBitmap + 1) * sectorsByBlock;
-			int firstDataBlock = (numBlocksINode + freeInodeBitmap + freeBlocksBitmap + 1);
-			int inodeSector = (1 + freeBlocksBitmap + freeInodeBitmap) * sectorsByBlock;
-
-			//Prepare root directory registers
-			struct t2fs_record rootDir1, rootDir2;
-			rootDir1.TypeVal = TYPEVAL_DIRETORIO;
-			rootDir1.name = "root";
-			rootDir1.inodeNumber = 0;
-
-			rootDir2.TypeVal = TYPEVAL_DIRETORIO;
-			rootDir2.name = "root";
-			rootDir2.inodeNumber = 0;
-
-			struct t2fs_inode rootNode; 
-			rootNode.blocksFileSize = 1;
-			rootNode.bytesFileSize = 2*sizeof(struct t2fs_record);
-			rootNode.dataPtr[0] = firstDataBlock;
-			rootNOde.dataPtr[1] = INVALID_PTR;	
-			rootNode.singleIndPtr = INVALID_PTR;  
-			rootNode.doubleIndPtr = INVALID_PTR;
-			rootNode.reservado[2] = {0,0};
-
-			buffer[0] = rootDir1.TypeVal;
-			buffer[1] = '\0';
-			strcat(buffer, rootDir1.name);
-			int temp;
-			sprintf(temp, "%i",rootDir1.inodeNumber);
-			strcat(buffer, temp);
-
-			int size = strlen(buffer);
-			buffer[size++] = rootDir2.TypeVal;
-			buffer[size] = '\0';
-			strcat(buffer, rootDir2.name)
-			sprintf(temp, "%i", rootDir2.inodeNumber);
-			strcat(buffer, temp);
-
-			if( (write_sector(firstDataSector, buffer)) )//Write root directory registers
-				return -1;
-
-			//Convert inode data to a buffer format(string)
-			sprintf(temp, "%i", rootNode.blocksFileSize);
-			strcpy(buffer, temp);
-			sprintf(temp, "%i", rootNode.bytesFileSize);
-			strcat(buffer, temp);
-			sprintf(temp, "%i", rootNode.dataPtr[0]);
-			strcat(buffer, temp);
-			sprintf(temp, "%i", rootNode.dataPtr[1]);
-			strcat(buffer, temp);
-			sprintf(temp, "%i", rootNode.singleIndPtr);
-			strcat(buffer, temp);
-			sprintf(temp, "%i", rootNode.doubleIndPtr);
-			strcat(buffer, temp);
-			sprintf(temp, "%i", rootNode.reservado[0]);
-			strcat(buffer, temp);
-			sprintf(temp, "%i", rootNode.reservado[1]);
-			strcat(buffer, temp);
-
-			if( (write_sector(inodeSector, buffer)) )//Write in the inode area
-				return -1;
-
-			if( !(setBitMap2(BITMAP_INODE, 0, 1)) )//inode 0 occupied
-				return -1;
-
-			if( !(setBitMap2(BITMAP_DADOS, firstDataBlock, 1)) )//block 0 occupied
-				return -1;
-
-
-		}
-
+	strncpy(superBlock->id,(char*) buffer, 4); //ID
+	if( strcmp(superBlock->id, "T2FS") ){
+		printf("Error, not T2FS\n");
+		return -1;
 	}
+	char temp[4];
+
+	strncpy(temp, buffer+4, 2);	//version
+	superBlock->version = (WORD)atoi(temp);
+
+	strncpy(temp, buffer+6, 2);	
+	superBlock->superblockSize = (WORD)atoi(temp);
+
+	strncpy(temp, buffer+8, 2);	
+	superBlock->freeBlocksBitmapSize =(WORD)atoi(temp);
+
+	strncpy(temp, buffer+10, 2);	
+	superBlock->freeInodeBitmapSize =(WORD)atoi(temp);
+
+	strncpy(temp, buffer+12, 2);	
+	superBlock->inodeAreaSize =(WORD)atoi(temp);
+
+	strncpy(temp, buffer+14, 2);	
+	superBlock->blockSize =(WORD)atoi(temp);
+
+	strncpy(temp, buffer+12, 2);	
+	superBlock->diskSize =(DWORD)atoi(temp);
+
+	bytesInBlock = superBlock->blockSize * SECTOR_SIZE;
+	firstDataBlock = (1 + superBlock->freeInodeBitmapSize + superBlock->freeBlocksBitmapSize + superBlock->inodeAreaSize);
+	inodeSector = (1 + superBlock->freeBlocksBitmapSize + superBlock->freeInodeBitmapSize)*SECTOR_SIZE;
+
+	///Initialise the root directory
+	currentDir = calloc(1, sizeof(dirDescription));
+	strcpy(currentDir->pathName, "/");
+	currentDir->block = firstDataBlock;
+
+	//First two records must be current directory and father directory
+	struct t2fs_record rootRecord0, rootRecord1;
+	rootRecord->TypeVal = TYPEVAL_DIRETORIO;
+	rootRecord->name = "root";
+	rootRecord->inodeNumber = 0;
+
+	rootRecord1->TypeVal = TYPEVAL_DIRETORIO;
+	rootRecord1->name = "root";
+	rootRecord1->inodeNumber = 0;
+
+	//Inode initialization
+	struct t2fs_inode rootInode0;
+	rootInode->blocksFileSize = 1;
+	rootInode->bytesFileSize = 2*sizeof(struct t2fs_record);
+	rootInode->dataPtr[0] = 0;
+	rootInode->dataPtr[1] = INVALID_PTR;
+	rootInode->singleIndPtr = INVALID_PTR;
+	rootInode->doubleIndPtr = INVALID_PTR;
+
+	//Now we set the bitmap of blocks and Inode
+
+	if( setBitMap2(BITMAP_INODE, 0, 1))
+		return -1; // FAILED
+
+	if( setBitMap2(BITMAP_DADOS, 0, 1))
+		return -1;
+
+	//We must write in the "disk"
+	memcpy(buffer, rootRecord0, sizeof(stuct t2fs_record));
+	memcpy(buffer + sizeof(struct t2fs_record), rootRecord1, sizeof(stuct t2fs_record));
+	write_sector( (firstDataBlock*SECTOR_SIZE), buffer);
+
+	memcpy(buffer, rootInode0, sizeof(struct t2fs_inode));
+	write_sector(inodeSector, buffer);
+	systemReady = 1;
+	return 0;
 }
+
