77d606cb62f0d4e4a50a813e7682265c7959b4c1
ifschilling <ianfschilling@gmail.com>
2018-06-09 23:27:22 -0300

    Backup codeanywhere

---
+++ functions.c
@@ -0,0 +1,566 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/t2fs.h"
+#include "../include/bitmap2.h"
+#include "../include/apidisk.h"
+#include "../include/functions.h"
+
+/* GLOBAL VARIABLES */
+dirDescription currentDir;
+int bytesInBlock;
+int firstDataBlock;
+int inodeSector;
+int inode_per_sector;
+DWORD PointersInBlock;
+
+
+
+struct coordinates getReg(char *path){
+	char *token;
+	char *nameaux;
+	struct t2fs_record dirReg;
+	struct coordinates info;
+	char buffer[SECTOR_SIZE];
+
+	switch(filenameDir(path)){
+		case 0: //diretorio pai
+		case 1: // diretorio atual
+			// ele vai pro diretório pai após ler o token e chamar o getRegByName
+			read_sector(blockFirstSector(currentDir.block), buffer);
+			memcpy(&info.record, buffer, sizeof(struct t2fs_inode));
+			break;
+		case 2: // root
+			read_sector(blockFirstSector(firstDataBlock), buffer);
+			memcpy(&info.record, buffer, sizeof(struct t2fs_inode));
+			break;
+	}
+
+	nameaux = (char *) malloc(strlen(path) +1);
+	strcpy(nameaux, path);
+	token = strtok(nameaux, "/");
+
+	while(token != NULL){
+		info = getReginDir(token, info.record);
+		token = strtok(NULL, "/");
+	}
+	return info;
+}
+
+int setReg(struct t2fs_record reg, char *path){
+struct coordinates info;
+struct t2fs_record regAux;
+DWORD buffer[SECTOR_SIZE];
+
+	//primeiro saber se o registro ja existe, ou se precisa ser criado
+	info = getReg(path);
+	if(info.record.TypeVal == TYPEVAL_INVALIDO){	//registro nao existe, precisa ser criado no diretorio pai
+	char *token;
+	char lastoken[59];
+	char *nameaux = malloc(strlen(path)+1);
+		
+		strcpy(nameaux, path);
+		token = strtok(nameaux, "/");
+		
+		whille(token != NULL){
+			token = strtok(NULL, "/");
+			strcpy(lastoken,token);
+		}
+		strncpy(nameaux, path, (strlen(path)-strlen(lastoken)+1));		//    /p1/p2/p5/p4/p6/texto
+		regAux = getReg(nameaux);
+		info = getNewRegCoordinates(regAux);
+		if(info.bytesread==-1){
+			printf("ERROR creating new record, directory is already full (funtion : setReg) \n");
+			return -1
+		}
+		if(read_sector(info.lastReadSector, buffer)){
+			printf("ERROR, reading sector (function: setReg) \n");
+			return -1;
+		}
+		memcpy(buffer + (info.lastAcessedPosition * sizeof(struct t2fs_record)), reg, sizeof(struct t2fs_record));
+		if(write_sector(info.lastReadSector, buffer)){
+			printf("ERROR, writing sector (function: setReg) \n");
+			return -1;
+		}
+	}else{
+		if(read_sector(info.lastReadSector, buffer)){
+			printf("ERROR, reading sector (function: setReg) \n");
+			return -1;
+		}
+		memcpy(buffer + (info.lastAcessedPosition * sizeof(struct t2fs_record)), reg, sizeof(struct t2fs_record));
+		if(write_sector(info.lastReadSector, buffer)){
+			printf("ERROR, writing sector (function: setReg) \n");
+			return -1;
+		}
+	}
+	return 0;
+}
+
+struct coordinates getReginDir(char *name, struct t2fs_record dirReg){
+int dirBlocks=0, offsetDirBlock=0, offsetSingleIndBlock=0, t=0, offsetDoubleIndBlock=0, h=0, g=0, j=0, z=0;
+DWORD rsector;
+DWORD pointer, indpointer;
+BYTE sindbuffer[SECTOR_SIZE];
+BYTE dindbuffer[SECTOR_SIZE];
+int bytesread=0;  //contabiliza o total de bytes que ja foram lidos
+struct t2fs_record regAux;
+struct t2fs_inode inodeAux;
+struct coordinates info;
+
+	
+	if(dirReg.TypeVal==TYPEVAL_DIRETORIO){
+		if(strcmp(name, ".") == 0){ // retorna o mesmo diretorio
+			return dirReg; 
+		}else if(strcmp(name, "..") == 0){  // retorna o diretorio pai
+			inodeAux = getInode(dirReg.inodeNumber);
+			if(inodeAux.bytesFileSize == -1){
+				regAux.TypeVal == TYPEVAL_INVALIDO;
+				return regAux;
+			}
+			read_sector(blockFirstSector(inodeAux.dataPtr[0]), sindbuffer);
+			memcpy(&regAux, (sindbuffer + sizeof(struct t2fs_inode)), sizeof(struct t2fs_inode));
+			return regAux;
+		}
+
+		inodeAux = getInode(dirReg.inodeNumber);
+		if(inodeAux.bytesFileSize == -1){
+			regAux.TypeVal == TYPEVAL_INVALIDO;
+			return regAux;
+		}
+		for(dirBlocks=0; dirBlocks < inodeAux.blocksFileSize; dirBlocks++){ //percorre blocos
+			if(dirBlocks==0)
+				rsector = (inodeAux.dataPtr[0] * superBlock->blockSize);
+			else if(dirBlocks==1)
+				rsector = (inodeAux.dataPtr[1] * superBlock->blockSize);
+			else if(dirBlocks<PointersInBlock){ //INDIRECAO SIMPLES
+				if(offsetSingleIndBlock < superblock->blockSize){
+					read_sector((blockFirstSector(inodeAux.singleIndPtr)) + offsetSingleIndBlock, sindbuffer);
+					offsetSingleIndBlock++;
+					t=0;
+
+					if(t < (PointersInBlock/superBlock->blockSize)){
+						memcpy(&pointer, sindbuffer+(t*sizeof(DWORD)), sizeof(DWORD));
+						rsector = (blockFirstSector(pointer));
+						t++;
+					}
+				}
+			}else{ //INDIRECAO DUPLA
+				if(offsetDoubleIndBlock<superBlock->blockSize){
+					read_sector((blockFirstSector(inodeAux.doubleIndPtr))+offsetDoubleIndBlock, dindbuffer);
+					offsetDoubleIndBlock++;
+					h=0;
+
+					if(h<(PointersInBlock/superBlock->blockSize)){
+						memcpy(&indpointer, dindbuffer + (h*sizeof(DWORD)), sizeof(DWORD));
+						h++;
+						j=0;
+
+						if(j<superblock->blockSize){
+							read_sector((blockFirstSector(indpointer)) + j, indbuffer);
+							j++;
+							g=0;
+
+							if(g<(PointersInBlock/superBlock->blockSize)){
+								memcpy(&pointer, indbuffer+(g*sizeof(DWORD)), sizeof(DWORD));
+								rsector = (blockFirstSector(pointer));
+								g++;
+							}
+						}
+					}
+				}
+			}
+			
+			for(offsetDirBlock=0; offsetDirBlock<superblock->blockSize; offsetDirBlock++){ //setores dentro do bloco
+				read_sector(rsector + offsetDirBlock, buffer);
+				z=0;
+				do{
+					memcpy(&regAux, buffer+(z*sizeof(struct t2fs_record)), sizeof(struct t2fs_record));
+					if(regAux.TypeVal == TYPEVAL_REGULAR || regAux.TypeVal == TYPEVAL_DIRETORIO){
+						bytesread += sizeof(struct t2fs_record);
+						if(strcmp(regAux.name, name)==0){
+							info.lastReadSector = rsector+offsetDirBlock;
+							info.lastAcessedPosition = z;
+							info.bytesread = bytesread;
+							info.record = regAux;
+							return info;
+						}
+					}
+					z++;
+				}while(z<(SECTOR_SIZE/sizeof(struct t2fs_record)) && bytesread<inodeAux.bytesFileSize);
+				if(bytesread >=inodeAux.bytesFileSize){
+					info.record.TypeVal = TYPEVAL_INVALIDO;
+					return info;
+				}
+			}
+		}
+	}
+	info.record.TypeVal = TYPEVAL_INVALIDO;
+	return info;
+}
+
+struct coordinates getNewRegCoordinates(struct t2fs_record dirReg){
+	int dirBlocks=0, offsetDirBlock=0, offsetSingleIndBlock=0, t=0, offsetDoubleIndBlock=0, h=0, g=0, j=0, z=0;
+	int plus =0;
+	DWORD rsector;
+	DWORD pointer, indpointer;
+	BYTE sindbuffer[SECTOR_SIZE];
+	BYTE dindbuffer[SECTOR_SIZE];
+	int bytesread =0;
+	struct t2fs_record regAux;
+	struct t2fs_inode inodeAux;
+	struct t2fs_record newReg;
+	struct coordinates info;
+
+	if(dirReg.TypeVal==TYPEVAL_DIRETORIO){
+		inodeAux = getInode(dirReg.inodeNumber);
+		if(inodeAux.bytesFileSize == -1){
+			regAux.TypeVal == TYPEVAL_INVALIDO;
+			return regAux;
+		}
+		
+		if(indeAux.bytesFileSize==(inodeAux.blocksFileSize * superblock->blockSize * SECTOR_SIZE)){	//ARQUIVO CHEIO
+			info.bytesread = -1;
+			return info
+		}
+		
+		newreg = getReg(path);
+		
+		if(((indeAux.bytesFileSize)%(inodeAux.blocksFileSize * superblock->blockSize * SECTOR_SIZE))==0)	//checa se o final dos dados coincide com o final de algum bloco
+			plus = 1;	
+			
+		for(dirBlocks=0; dirBlocks < (inodeAux.blocksFileSize+plus); dirBlocks++){ //percorre blocos
+			if(dirBlocks==0)
+				rsector = (inodeAux.dataPtr[0] * superBlock->blockSize);
+			else if(dirBlocks==1)
+				rsector = (inodeAux.dataPtr[1] * superBlock->blockSize);
+			else if(dirBlocks<PointersInBlock){ //INDIRECAO SIMPLES
+				if(offsetSingleIndBlock < superblock->blockSize){
+					read_sector((blockFirstSector(inodeAux.singleIndPtr)) + offsetSingleIndBlock, sindbuffer);
+					offsetSingleIndBlock++;
+					t=0;
+
+					if(t < (PointersInBlock/superBlock->blockSize)){
+						memcpy(&pointer, sindbuffer+(t*sizeof(DWORD)), sizeof(DWORD));
+						rsector = (blockFirstSector(pointer));
+						t++;
+					}
+				}
+			}
+			else{ //INDIRECAO DUPLA
+				if(offsetDoubleIndBlock<superBlock->blockSize){
+					read_sector((blockFirstSector(inodeAux.doubleIndPtr))+offsetDoubleIndBlock, dindbuffer);
+					offsetDoubleIndBlock++;
+					h=0;
+
+					if(h<(PointersInBlock/superBlock->blockSize)){
+						memcpy(&indpointer, dindbuffer + (h*sizeof(DWORD)), sizeof(DWORD));
+						h++;
+						j=0;
+						
+						if(j<superblock->blockSize){
+							read_sector((blockFirstSector(indpointer)) + j, indbuffer);
+							j++;
+							g=0;
+
+							if(g<(PointersInBlock/superBlock->blockSize)){
+								memcpy(&pointer, indbuffer+(g*sizeof(DWORD)), sizeof(DWORD));
+								rsector = (blockFirstSector(pointer));
+								g++;
+							}
+						}
+					}
+				}
+			}
+
+			for(offsetDirBlock=0; offsetDirBlock<superblock->blockSize; offsetDirBlock++){ //setores dentro do bloco
+				read_sector(rsector + offsetDirBlock, buffer);
+				z=0;
+				do{
+					memcpy(&regAux, buffer+(z*sizeof(struct t2fs_record)), sizeof(struct t2fs_record));
+					if(regAux.TypeVal !== TYPEVAL_INVALIDO)
+						bytesread += sizeof(struct t2fs_record);	
+							if(regAux.TypeVal == TYPEVAL_INVALIDO){
+									info.lastReadSector = rsector+offsetDirBlock;
+									info.lastAcessedPosition = z;
+									info.bytesread = bytesread;
+									info.record = regAux;
+									return info;
+								}else if(bytesread>inodeAux.bytesFileSize){		//se nao houver registros invalidos retornara as "coordenadas apontando" para uma posicao após o ultimo registro valido
+									info.lastReadSector = rsector+offsetDirBlock;
+									info.lastAcessedPosition = z;
+									info.bytesread = bytesread;
+									return info;
+								}
+					z++;
+				}while(z<(SECTOR_SIZE/sizeof(struct t2fs_record)));
+			}
+		}
+	}
+}
+
+struct t2fs_inode getInode(DWORD inodeNumber){
+int blockOffset = (int) (inodeNNumber/inodes_per_sector);
+int sectorOffset = (int) (inodeNumber % inodes_per_sector);
+struct t2fs_inode inode;
+
+	if(read_sector(inodeSector + blockOffset, buffer)){
+			printf("read_sector ERROR! (Function: getInode) \n");
+			inode.bytesFileSize = -1;
+			return inode;
+	}
+	memcpy(&inode, buffer + sectorOffset, sizeof(struct t2fs_inode));
+
+	return inode;
+}
+
+int setInode(struct t2fs_inode inode, DWORD inodeNumber){
+DWORD iSector, iPosition;
+iSector = inodeSector + (inodeNumber/inode_per_sector);
+iPosition = inodeNumber % inode_per_sector;
+
+	unsigned char iBuf[SECTOR_SIZE];
+	if(getBitMap2(BITMAP_INODE, inodeNumber)==0) //checa se o inode a ser modificado, "ja existe/esta ocupado"
+			if(setBitMap2(BITMAP_INODE, inodeNumber, 1))
+					printf("ERROR, trying to update inode bitmap! (Function: modifyInode) \n");
+
+	if( !read_sector(iSector, iBuf) )
+	{
+			printf("ERROR reading sector: %i in modifyInode \n", iSector);
+			return -1;
+	}
+	/*  Now iBuf has our iNode and others as well, we must modify only the one we want */
+	/*  e.g inode number 14, if 3 per sector 14/3 = 4; 14%3 = 1 so sector 4, struct 1*/
+	memcpy(iBuf + (iPosition*sizeof(t2fs_inode)), inode, sizeof(t2fs_inode));
+	if( !write_sector(iSector, iBuf) )
+	{
+			printf("ERROR writing sector %i in modifyInode", iSector);
+			return -1;
+	}
+	return 0;
+}
+
+int absolutePath(char *abspath, char *path){
+	char fullpathname[MAX_FILE_NAME_SIZE +1];
+	char name[100][59], *pathaux, *token, *current;
+	int cont = 0;
+	pathaux = (char *) malloc(strlen(path) +1);
+	strcpy(pathaux, path);
+	switch(filenameDir(pathaux)){
+		case 0: //diretorio pai
+		case 1: // diretorio atual
+			// separa o diretorio corrente em tokens e os passa para name
+			current = (char *) malloc(strlen(currentDir.pathName) +1);
+			strcpy(current, currentDir.pathName);
+			token = strtok(current, "/");
+			while(token != NULL){
+				strcpy(name[cont], token); // passa o token pra uma string do nome
+				cont++;
+				token = strtok(NULL, "/");
+			}
+			break;
+		case 2: // root
+			// faz nada, pois o root começa vazio
+			break;
+	}
+	
+	token = strtok(pathaux, "/");
+	while(token != NULL){
+		if(strcmp(token, ".") == 0){
+			// se é o diretorio atual, faz nada
+		}
+		else if(strcmp(token, "..") == 0){
+			// diretorio pai, apaga o ultimo name salvo
+			if(cont > 0){
+				cont--;
+			}	
+		}
+		else{
+			strcpy(name[cont], token); // passa o token pra uma string do nome
+			cont++;
+		}
+		token = strtok(NULL, "/");
+	}
+	
+	//concatena todo o caminho em fullpathname
+	int i;
+	fullpathname[0] = '\0';
+	for(i=0; i<cont; i++){
+		strcat(fullpathname, "/");
+		strcat(fullpathname, name[i]);
+	}
+	if(strlen(fullpathname) == 0){
+		strcpy(fullpathname, "/");
+	}
+	
+	strcpy(abspath, fullpathname);
+	return 0;
+}
+
+int filenameDir(char *filename){
+	if(filename[0]=='/'){
+  	return 2;
+  }
+  else if(filename[0]=='.' && filename[1]=='.' && filename[2]=='/'){
+	  return 0;
+  }
+  else{
+  	return 1;
+  }
+}
+
+unsigned int blockFirstSector(unsigned int block){
+	return (block * superBlock->blockSize);
+}
+
+int delBlocks(DWORD inode, int cPointer){
+  
+  //read inode 
+  t2fs_inode delNode= getInode(inode);
+  DWORD blocksRemaining = delNode.blocksFileSize;
+  DWORD iBSector = SECTOR_SIZE/sizeof(DWORD);/*pointers by sector*/
+
+  DWORD dBlocksToDel;
+  BYTE delBuff;
+  DWORD indexSectorsToRead;
+  DWORD sFirstBlockSector;
+  DWORD bIndex, bDataFinalPosition;
+  dBlocksToDel = blocksRemaining;
+
+  DWORD arrayOfDataBlocks[dBlocksToDel];
+  
+  int i, j;
+  int k, l;
+  /*-------------------------------------------------------------------------------*/
+  DWORD remainingBytes = delNode.bytesFileSize - (cPointer+2);//bytes to delete
+  DWORD blockOfPointer = cPointer/bytesInBlock;
+  if( (cPointer % bytesInBlock) != 0)
+  	blockOfPointer++;
+  
+  /*-------------------------------------------------------------------------------*/
+ 
+      // From end to the first block
+  if(blocksRemaining > (2 + PointersInBlock)){ /* double Indirection */
+  	if(cPointer == -1){/*DELETE*/
+	    DWORD dfirstBlockSector = blockFirstSector(delNode.doubleIndPtr);
+	    dBlocksToDel = blocksRemaining - ( 2 + PointersInBlock);
+
+	   bIndex = (dBlocksToDel / PointersInBlock) + 1;/*double indirection block of index*/
+	   bDataFinalPosition = dBlocksToDel % PointersInBlock;/*Last valid block position*/
+
+	    /*We have to read just the correct amount of block index*/
+	    indexSectorsToRead = bIndex /iBSector;//
+	    if( (bIndex % iBSector) != 0 )
+	    	indexSectorsToRead++;
+	   
+	    /*array of blocks of index and their dataBlocks*/
+	    DWORD arrayOfBlockIndex[bIndex];
+
+	    k = 0;
+	    for(i = 0; i < indexSectorsToRead; i++){/*block of index in each sector*//
+	    	if( read_sector(dfirstBlockSector + i, &delBuff) ){
+		    	printf("Error in delBlocks,failed to read sector %i\n", dfirstBlockSector+i);
+		    	return -1;
+		    }
+		    for(j = 0; j<iBSector;j++){
+		    	if(k < bIndex){
+		    		memcpy(&arrayOfBlockIndex[k], delBuff+(sizeof(DWORD)*j), sizeof(DWORD));/*This is the block of indexes*/
+		    		k++;
+		    	}
+		    }
+		  }
+			/* We have all the blocks of index */
+		  l = 0;
+		  for( i = 0; i < bIndex, i++){/*Each index block*/
+		  	sFirstBlockSector = blockFirstSector(arrayOfBlockIndex[i]);/*First Block of the single index block*/
+		  	for(j = 0; j < iBSector; j++){
+		  		if( read_sector(sFirstBlockSector + j, &delBuff) ){
+		    		printf("Error in delBlocks,failed to read sector %i\n", sFirstBlockSector+j);
+		    		return -1;
+		    	}
+		    	for(k = 0; k <iBSector; k++){
+		    		if(l < dBlocksToDel){
+		    			memcpy(&arrayOfDataBlocks[l], delBuff + (k*sizeof(DWORD)), sizeof(DWORD));
+		    			l++;
+		    		}
+		    	}
+		  	}
+		  }
+
+		  /*Now we have to free all the blocks*/
+		  for(i = 0; i< bIndex; i++){
+		  	if ( setBitmap2(BITMAP_DADOS,arrayOfBlockIndex[i], 0) ){
+          printf("ERROR setting bits in delBlocks\n");
+          return -1;
+        }
+		  }
+      
+      for(i = 0; i< dBlocksToDel; i++){
+		  	if ( setBitmap2(BITMAP_DADOS,arrayOfDataBlocks[i], 0) ){
+          printf("ERROR setting bits in delBlocks\n");
+          return -1;
+        }
+		  }
+		  if ( setBitmap2(BITMAP_DADOS,delNode.doubleIndPtr, 0) ){
+          printf("ERROR setting bits in delBlocks\n");
+          return -1;
+        }
+      blocksRemaining -= dBlocksToDel;
+	  }
+	  else{/*Truncate*/
+      
+	  }	  	
+  }
+
+  /*Single indirection*/
+  if(blocksRemaining > 2){
+    dBlocksToDel = blocksRemaining - 2;
+		sFirstBlockSector = blockFirstSector(delNode.singleIndPtr);/*First Block of the single index block*/
+
+    indexSectorsToRead = dBlocksToDel /iBSector;//
+    if( (bIndex % iBSector) != 0 )
+    	indexSectorsToRead++;
+
+    l = 0;
+  	for(j = 0; j < indexSectorsToRead; j++){
+  		if( read_sector(sFirstBlockSector + j, &delBuff) ){
+    		printf("Error in delBlocks,failed to read sector %i\n", sFirstBlockSector+j);
+    		return -1;
+    	}
+    	for(k = 0; k <iBSector; k++){
+    		if(l < dBlocksToDel){
+    			memcpy(&arrayOfDataBlocks[l], delBuff + (k*sizeof(DWORD)), sizeof(DWORD));
+    			l++;
+    		}
+    	}
+  	}
+
+  	/*Release the blocks*/
+  	if ( setBitmap2(BITMAP_DADOS,delNode.singleIndPtr, 0) ){
+      printf("ERROR setting bits in delBlocks\n");
+      return -1;
+    }
+
+  	for(i = 0; i< dBlocksToDel; i++){
+		  if ( setBitmap2(BITMAP_DADOS,arrayOfDataBlocks[i], 0) ){
+        printf("ERROR setting bits in delBlocks\n");
+        return -1;
+      }
+		}
+      blocksRemaining -= dBlocksToDel;
+    
+  }
+  /*Direct blocks*/
+  if(cPointer == -1){/*DELETE ALL*/
+  	if( setBitmap2(BITMAP_DADOS, delNode.dataPtr[0], 0) ){
+  		printf("ERROR setting bits in delBlocks\n");
+  		return -1;
+  	}
+  	if( setBitmap2(BITMAP_DADOS, delNode.dataPtr[1], 0) ){
+  		printf("ERROR setting bits in delBlocks\n");
+  		return -1;
+  	}
+  }
+  
+  return 0;
+  
+}
---
+++ functions.h
@@ -0,0 +1,124 @@
+/*************************************************************************
+
+	Funções auxiliares usadas pelo T2FS
+	
+	Versão: 16.2
+	
+*************************************************************************/
+
+#ifndef __functions_h__
+#define __functions_h__
+
+typedef struct cD
+{
+    char pathName[MAX_FILE_NAME_SIZE +1];
+    int block;
+} dirDescription;
+
+struct coordinates{
+	unsigned int lastAcessedPosition;
+	unsigned int lastReadSector;
+	int bytesread;
+	struct t2fs_record record;
+};
+
+//GETTERS E SETTERS
+/*-----------------------------------------------------------------------------
+Função: Acha um registro que tenha o mesmo nome do último argumento de regName.
+Entrada: path -> Caminho absoluto ou relativo do registro(onde o ultimo argumento sera o nome do registro a ser procurado).
+Saída: Se a operação foi realizada com sucesso, a função retorna a struct coordinates com o registro do arquivo encontrado, junto com o ultimo setor acessado, ultima posicao acessada e numero de bytes lidos.
+		    Em caso de erro, será retornado o registro com TypeVal = TYPEVAL_INVALIDO. (testar struct coordinates.registro.Typeval == TYPEVAL_INVALIDO)
+Exemplo: getReg("/p1/p2/p3") => retorna o registro do diretorio p3
+        getReg("/p1/p2/p3/texto.txt") => retorna o registro do arquivo texto.txt
+				getReg("../p2/p3/texto.txt") => retorna o registro do arquivo texto.txt
+-----------------------------------------------------------------------------*/
+struct coordinates getReg(char *path);
+
+/*-----------------------------------------------------------------------------
+Função: Copia os dados do registro reg para o registro dado por abspath.
+Entrada: abspath -> Caminho absoluto do registro que será alterado ou criado (o último argumento é o nome do registro a ser alterado).
+          reg -> estrutura com os campos que serão copiados para o registro apontado por regname
+Saída: Se a operação foi realizada com sucesso, a função retorna 0.
+		    Em caso de erro, será retornado -1.
+Exemplo: setReg(reg, "/p1/p2/p3") => alterará o registro do diretorio p3
+        setReg(reg, "/p1/p2/p3/texto.txt") => alterará o registro do arquivo texto.txt
+-----------------------------------------------------------------------------*/
+int setReg(struct t2fs_record reg, char *abspath);
+
+/*-----------------------------------------------------------------------------
+Função:	Procura um registro VALIDO com nome igual a name, no diretorio apontado por dirReg
+Entra:	name -> nome do arquivo a ser procurado.
+        Se for '.' retorna o mesmo diretorio.
+        Se for '..' retorna o diretório pai.
+        dirReg -> Registro do diretorio onde o a busca sera feita
+Saída:	Se a operação foi realizada com sucesso, a função retorna a struct coordinates com o registro do arquivo encontrado, junto com o ultimo setor acessado, ultima posicao acessada e numero de bytes lidos.
+		    Em caso de erro, será retornado o registro dentro da struct coordinates com TypeVal = TYPEVAL_INVALIDO.
+-----------------------------------------------------------------------------*/
+struct coordinates getReginDir(char *name, struct t2fs_record dirReg); // antigo getRegByName
+
+/*-----------------------------------------------------------------------------
+Função:	Acha o ultimo registro valido ou o primeiro registro invalido no diretorio apontado por dirReg
+Entra:	dirReg -> Registro do diretorio onde o a busca sera feita
+Saída:	Retorna uma estrutura coordinates, contendo a ultima posicao e o ultimo setor acessado (setor e posicao onde sera criado o novo registro);
+-----------------------------------------------------------------------------*/
+struct coordinates getNewRegCoordinates(struct t2fs_record dirReg);
+
+/*-----------------------------------------------------------------------------
+Função:	Procura inode com o índice de inodeNumber
+Entra:	inodeNumber -> índice do inode a ser retornado.
+Saída:	sucesso, retorna o inode
+        erro de leitura do setor, retorna blocksFileSize = -1 (obs: testar se é igual a -1, se testar se é menor que 0 da erro)
+-----------------------------------------------------------------------------*/
+struct t2fs_inode getInode(DWORD inodeNumber);
+
+/*-----------------------------------------------------------------------------
+Função: Inode de índice inodeNumber vira uma cópia da estrutura inode passada
+Entrada: inodeNum -> número do inode
+         inode -> estrutura do inode que sera copiada para o inode apontado por inodeNumber
+          
+Saída: Retorna 0 se for realizada com sucesso, senão retorna 1.
+-----------------------------------------------------------------------------*/
+int setInode(struct t2fs_inode, DWORD inodeNumber);
+
+
+
+//MANIPULACAO DE CAMINHO
+/*-----------------------------------------------------------------------------
+Função: Recebe um caminho e retorna esse caminho na forma absoluta
+Entrada: path-> caminho a ser transformado
+          
+Saída: abspath: devolve o caminho absoluto se converter com sucesso
+retorna 0 se converteu com sucesso, senao retorna 1
+-----------------------------------------------------------------------------*/
+int absolutePath(char *abspath, char *path);
+
+
+
+// AUXILIARES
+/*---------------------------------------------------------------------------------
+Função: Retorna o tipo do primeiro diretório de filename
+Entrada: filename -> nome do arquivo ou diretório
+Saída: Se a operação for realizada com sucesso, retorna
+        0 -> diretório pai
+        1 -> diretório atual
+        2 -> root
+-----------------------------------------------------------------------------------*/
+int filenameDir(char *filename);
+
+/*---------------------------------------------------------------------------------
+Função: Retorna o primeiro setor correspondente ao bloco
+Entrada: block -> numero do bloco
+Saída: retorna sempre o primeiro setor do bloco
+-----------------------------------------------------------------------------------*/
+unsigned int blockFirstSector(unsigned int block);
+
+/*-----------------------------------------------------------------------------------
+Função: Percorre os blocos que estão sendo utilizados pelo inode e libera eles
+Entrada:  inode -> número do inode do arquivo 
+          cPointer -> Se -1 apaga todos os bytes, se outro valor maior que zero apaga 
+          a partir do "cPointer"(inclusive)
+Saída: Retorna 0 se for realizada com sucesso, senão retorna um valor negativo
+-----------------------------------------------------------------------------------*/
+int delBlocks(DWORD inode, int cPointer);
+
+#endif
